184c7f33f53626f906dedf3fb3657b7afa421fce
jiwonpark831 <jiwonpark23@handong.ac.kr>
2024-04-06 19:18:41 +0900

    ds004 update

--- lab5/ds004.cpp
+++ lab5/ds004.cpp
@@ -10,31 +10,27 @@ struct st_score
 
 int main()
 {
-    int kor, eng, math;
     st_score score;
-    cin >> kor >> eng >> math;
-    score.kor = kor;
-    score.eng = eng;
-    score.math = math;
+    cin >> score.kor >> score.eng >> score.math;
 
-    score.total = kor + eng + math;
+    score.total = score.kor + score.eng + score.math;
     score.average = score.total / 3.0;
 
     string kor_poff;
     string eng_poff;
     string math_poff;
 
-    if (kor > 70)
+    if (score.kor > 70)
         kor_poff = "Pass";
     else
         kor_poff = "Fail";
 
-    if (eng > 70)
+    if (score.eng > 70)
         eng_poff = "Pass";
     else
         eng_poff = "Fail";
 
-    if (math > 70)
+    if (score.math > 70)
         math_poff = "Pass";
     else
         math_poff = "Fail";
