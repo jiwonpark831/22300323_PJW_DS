7e4a20f342b5192d4512c946651eec06b17a8ebc
jiwonpark831 <jiwonpark23@handong.ac.kr>
2024-05-19 18:45:49 +0900

    ds051

--- lab11/MyCircularQueue.cpp
+++ lab11/MyCircularQueue.cpp
@@ -19,6 +19,8 @@ public:
     };
     void initialize();
     void enqueue(const elem &e);
+    void printDetail() const;
+    int size() const;
     void dequeue();
     bool isEmpty() const;
     bool isFull() const;
@@ -29,8 +31,8 @@ public:
 
 MyCircularQueue::MyCircularQueue(int s)
 {
-    maxsize = s;
-    list = new elem[s];
+    maxsize = s + 1;
+    list = new elem[maxsize];
     initialize();
 }
 void MyCircularQueue::initialize()
@@ -40,7 +42,50 @@ void MyCircularQueue::initialize()
 }
 void MyCircularQueue::enqueue(const elem &e)
 {
-    int size() const;
+    if (!isFull())
+    {
+        list[rear] = e;
+        rear = (rear + 1) % maxsize;
+    }
+    else
+        cout << "Queue is Full!" << endl;
+}
+void MyCircularQueue::printDetail() const
+{
+    cout << "Queue: ";
+    if (!isEmpty())
+    {
+        int i = front;
+        while (i != rear)
+        {
+            cout << "[" << list[i] << "]";
+            i = (i + 1) % maxsize;
+        }
+    }
+};
+int MyCircularQueue::size() const
+{
+    int size = 0;
+    if (!isEmpty())
+    {
+        int i = front;
+        while (i != rear)
+        {
+            size++;
+            i = (i + 1) % maxsize;
+        }
+    }
+    return size;
+}
+void MyCircularQueue::dequeue()
+{
+
+    if (!isEmpty())
+    {
+        front = (front + 1) % maxsize;
+    }
+    else
+        cout << "Queue is Empty!" << endl;
 }
 bool MyCircularQueue::isEmpty() const
 {
@@ -56,15 +101,60 @@ bool MyCircularQueue::isFull() const
     return false;
 }
 
+void MyCircularQueue::print()
+{
+    /*cout << "Queue: ";
+    if (!isEmpty())
+    {
+        int i = front;
+        while (i != rear)
+        {
+            cout << "[" << list[i] << "]";
+            i = (i + 1) % maxsize;
+        }
+    }*/
+    cout << "Size: " << size() << endl;
+    printDetail();
+    cout << "\nindex: ";
+    if (!isEmpty())
+    {
+        int i = front;
+        while (i != rear)
+        {
+            cout << i + 1 << " ";
+            i = (i + 1) % maxsize;
+        }
+    }
+    cout << "\nfront: " << front << ", rear: " << rear << endl;
+}
+
 int main()
 {
-    int s;
-    cout << "Size: ";
-    cin >> s;
-    MyCircularQueue(s);
-    cout << "Queue: [" << "]" << endl;
-    cout << "index: " << endl;
-    cout << "front: " << ", rear: " << endl;
+    MyCircularQueue queue(9);
+    cout << "===== Dequeue x 1 =====" << endl;
+    queue.dequeue();
+    queue.print();
+    cout << "===== Enqueue x 7 (10~70) =====" << endl;
+    queue.enqueue(10);
+    queue.enqueue(20);
+    queue.enqueue(30);
+    queue.enqueue(40);
+    queue.enqueue(50);
+    queue.enqueue(60);
+    queue.enqueue(70);
+    queue.print();
+    cout << "===== Dequeue x 3 =====" << endl;
+    for (int i = 0; i < 3; i++)
+        queue.dequeue();
+    queue.print();
+    cout << "===== Enqueue x 6 (10~60) =====" << endl;
+    queue.enqueue(10);
+    queue.enqueue(20);
+    queue.enqueue(30);
+    queue.enqueue(40);
+    queue.enqueue(50);
+    queue.enqueue(60);
+    queue.print();
 
     return 0;
 }
